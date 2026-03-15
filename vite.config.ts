6662c4473ece58ca9b2281a3dbb3dfd86ec36c71
Alexander Gotsulov <agotsulov@gmail.com>
2026-03-15 18:07:29 +0600

    fix: win-55-ui 0.2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { win55ui } from 'win-55-ui-vue/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), win55ui()],
+  resolve: {
+    alias: {
+      'win-55-ui-vue/style.css': resolve(__dirname, 'node_modules/win-55-ui-vue/dist/index.css'),
+    },
+  },
 })
